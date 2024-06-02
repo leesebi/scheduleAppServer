817b9f97caee44949c66623405f654422166e67f
이세비 <lsy205105@naver.com>
2024-06-02 15:45:13 +0900

    api 경로 수정

--- src/main/java/com/example/scheduleappserver/controller/CommentController.java
+++ src/main/java/com/example/scheduleappserver/controller/CommentController.java
@@ -14,25 +14,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/schedule/{scheduleId}/comments")
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService service;
-    @PostMapping("/schedule/{scheduleId}/comments")
+    @PostMapping
     public void createComment(@PathVariable Long scheduleId, @Valid @RequestBody CommentRequestDto requestDto,
                               ServletRequest request){
         User user = (User) request.getAttribute("user");
         service.save(scheduleId, requestDto, user);
     }
 
-    @PutMapping("/schedule/{scheduleId}/comments/{commentId}")
+    @PutMapping("/{commentId}")
     public CommentResponseDto updateComment(@PathVariable Long scheduleId, @PathVariable Long commentId,
                                             @RequestBody CommentUpdateRequestDto requestDto, ServletRequest request){
         User user = (User) request.getAttribute("user");
         return service.update(scheduleId, commentId, requestDto, user);
     }
 
-    @DeleteMapping("/schedule/{scheduleId}/comments/{commentId}")
+    @DeleteMapping("/{commentId}")
     public ResponseEntity<CommentResponseDto> deleteComment(@PathVariable Long scheduleId, @PathVariable Long commentId,
                                                             ServletRequest request){
         User user = (User) request.getAttribute("user");
